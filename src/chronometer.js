942bcde91335c465c0f1afcb24b8bee218c08704
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-10-11 17:54:18 +0200

    iteration 1-7

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,62 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+     
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+     
+
+    setInterval(() => {
+      this.currentTime += 1
+      
+      console.log(this)
+      if (typeof printTimeCallback == "function") {
+      printTimeCallback()}
+    },1000)
+     
   }
 
+
   getMinutes() {
-    // ... your code goes here
+    
+
+    console.log(this)
+    return Math.floor(this.currentTime/60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
+  
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    String(this.currentTime).slice(0,2)
+    
+    
+      return "0" + this.currentTime
+
+      if (this.currentTime >= 9) {
+    return this.currentTime
+    
+      }
+    
+    
+
+  
   }
+
+  
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.currentTime);
+    
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
